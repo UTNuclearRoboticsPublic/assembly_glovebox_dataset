4a80b847ed80be3761c168c441b5a78e45af34d8
Shivansh <106922533+shondle@users.noreply.github.com>
2023-05-08 17:33:05 -0500

    Add colormap

--- convert.py
+++ convert.py
@@ -3,61 +3,48 @@ from PIL import Image
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import glob
 
 def convert(path_to_images, path_to_labels):
     labels = os.listdir(path_to_labels)
-    print(f"the labels are {labels}")
-    for index, label in enumerate(labels):
+    images = os.listdir(path_to_images)
+    image = Image.open(f"{path_to_images}/{images[0]}")
+    height = np.asarray(image).shape[0]
+    width = np.asarray(image).shape[1]
 
-        task_number = label.split("task-")[1].split("-")[0]
-        mask = Image.open(f"{path_to_labels}/{label}")
-        mask = mask.convert("RGB")
-
-        mask_arr = np.array(mask)
-
-        if "Banana" in label:
-            mask_arr[:, :, 0] = 0
-            mask_arr[:, :, 2] = 0
-
-        if "Orange" in label:
-            mask_arr[:, :, 0] = 0
-            mask_arr[:, :, 1] = 0
+    last_task = labels[-1].split("task-")[1].split("-")[0]
 
 
-        mask_arr[mask_arr!=255] = 0
+    color_map = {
+        0: (0, 0, 0),
+        1: (0, 255, 0),
+        2: (0, 0, 255)
+    }
 
-        print(f"the index is {index} and {label[index+1]}")
-        try:
-            next_task = labels[index+1].split("task-")[1].split("-")[0]
-        except:
-            print("reached end")
-            next_task = -1
-
-        if task_number == next_task:
-            new_label = labels[index+1]
-            new_mask = Image.open(f"{path_to_labels}/{new_label}")
-            new_mask = new_mask.convert("RGB")
-            new_mask_arr = np.array(np.asarray(new_mask))
-
-            print(f"the shape of new is {new_mask_arr.shape}")
-
-            if "Banana" in new_label:
-                new_mask_arr[:, :, 0] = 0
-                new_mask_arr[:, :, 2] = 0
-            elif "Orange" in new_label:
-                new_mask_arr[:, :, 1] = 0
-                new_mask_arr[:, :, 2] = 0
-            
-            mask_arr = new_mask_arr + mask_arr
-
-            mask_arr[(mask_arr!=255)] = 0
-
-            print(np.unique(mask_arr))
-            new_label = Image.fromarray(mask_arr)
-
-            plt.imshow(new_label)
-            new_label.save(f"./masks/task-{task_number}.png")
-            plt.show()
+    for i in range(int(last_task)+1):
+        # print(labels)
+        filtered_labels = [f for f in labels if f.startswith(f"task-{i}")]
+        final_arr = np.empty([height, width])
+        for index, label in enumerate(filtered_labels):
+            print(f"in the for loop")
+            mask = Image.open(f"{path_to_labels}/{label}")
+            mask_arr = np.array(np.asarray(mask))
+            if "Banana" in label:
+                mask_arr[mask_arr>0] = 1
+            if "Orange" in label:
+                mask_arr[mask_arr>0] = 2
+            final_arr+=mask_arr
+        # final_label = Image.fromarray(mask_arr)
+        image = np.zeros((height, width, 3), dtype=np.uint8)
+        for idx, color in color_map.items():
+            image[final_arr == idx] = color
+        image = Image.fromarray(image)
+        # image = image.convert('L')
+        
+        if np.any(final_arr > 0):
+            image.save(f"masks/task-{i}.png")
+    plt.imshow(final_arr)
+    plt.show()
 
 if __name__ == "__main__":
     convert('./images', './raw')
