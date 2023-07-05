434e8e0109447b6722da75ac38de30f73ed426f3
shondle <106922533+shondle@users.noreply.github.com>
2023-07-05 11:30:34 -0500

    Add vectorized ACE

--- training/metrics.py
+++ training/metrics.py
@@ -23,51 +23,97 @@ def expected_calibration_error(preds, targets):
     return ace
 
 def adaptive_calibration_error(y_true, y_pred, num_bins=15):
-    pred_y = np.argmax(y_pred, axis=-1)
-    correct = (pred_y == y_true).astype(np.float32)
-    prob_y = np.max(y_pred, axis=-1)
+    """
+    Overall structure
 
+    Flatten and sort values in each class array out of the softmax -> [2, 3, 900]
+    Flatten and sort the values in the ground truth tensor as well -> [2, 3, 900]
+    Flatten and sort the values in the argmaxes predictions -> [2, 3, 900]
+    ^ all three should have matching values from the input image for accuracy predictions
 
-    b = np.linspace(start=0, stop=1.0, num=num_bins)
-    b = np.quantile(prob_y, b)
-    b = np.unique(b)
-    num_bins = len(b)
-    bins = np.digitize(prob_y, bins=b, right=True)
+    bin these values into 15 bins, so resulting shape for each is [2, 3, 15, 60] where each
+    indice of the 15 bins contains the respective sorted probabilities for that bin (do the same for the target and argmaxed tensor)
 
+    calculate confidences for each bin
+    calculate accuracy for each bin
+    sum across classes, then sum across bins, then across num_examples, and divide by (num_ranges * num_examples * num_classes)
+    """
 
-    o = 0
-    for b in range(num_bins):
-        mask = bins == b
-        if np.any(mask):
-            o += np.abs(np.sum(correct[mask] - prob_y[mask]))
+    """## Initializing Inputs"""
 
-    return o / y_pred.shape[0]
+    probs = torch.softmax(y_pred, dim=1)
+    confidences, y_hats = torch.max(probs, dim=1) # respective values (max value along each row), indices (argmax return)
 
-# def adaptive_calibration_error2(y, raw_preds, ranges=15):
+    num_classes = y_pred.shape[1]
 
-#     predictions = torch.argmax(raw_preds, dim=1) # shape [B, N, W, H]
-#     probs = torch.softmax(raw_preds, dim=1)
-#     # y - shape [B, N, W, H]
+    """## Sorting softmax prediction and ground truth values (BEFORE BINNING)"""
 
-#     # 1/KR
-#     classes = raw_preds.shape[1]
-#     factor = 1 / (15 * classes)
+    num_ranges = 15
 
-#     # [N/R] for each image
-#     range_idx = torch.numel(predictions) / torch.shape[0]
+    flattened = torch.flatten(probs, start_dim=2)
+    sorted_pred, sort_indices = torch.sort(flattened, dim=1)
+    sorted_pred.shape
 
-#     for img in range(raw_preds.shape[0]):
-#     # for each class
-#         for c in range(classes):
-#             preds = predictions[img, c, :, :]
-#             one_dim_preds = torch.flatten(preds)
-            
-#             # figure out how to do bins here...
-#             N = torch.numel(predictions[img, c, :, :])
-#             range_iter = torch.linspace(N, ranges)
+    """Doing this for the ground truth, using the indices from sorting the probabilities to sort this correctly"""
 
-#             # for each bin
-#             for r in range_iter:
-#                 one_dim_preds[]
-#                 pass
+    y_true.shape
+    ground = torch.nn.functional.one_hot(y_true, num_classes=3)
+    ground_truth = ground.permute(0,-1, 1, 2)
+
+    flat_gt = torch.flatten(ground_truth, start_dim=2)
+
+    sorted_gt = torch.gather(flat_gt, dim=1, index=sort_indices)
+    sorted_gt.shape
+
+    """Doing this for the argmaxed predictions (using indices from sorted probabilities)"""
+
+    max_ground = torch.nn.functional.one_hot(y_hats, num_classes=3)
+    ground_arg = max_ground.permute(0,-1, 1, 2)
+    flat_arg = torch.flatten(ground_arg, start_dim=2)
+    sorted_arg = torch.gather(flat_arg, dim=1, index=sort_indices)
+    sorted_arg.shape
+
+    """## Binning softmax, ground truth values"""
+
+    chunked_preds = torch.chunk(sorted_pred, 15, dim=2)
+    binned_preds = torch.stack(chunked_preds, dim=2)
+    binned_preds.shape
+
+    chunked_gt = torch.chunk(sorted_gt, 15, dim=2)
+    binned_gt = torch.stack(chunked_gt, dim=2)
+    binned_gt.shape
+
+    chunked_arg = torch.chunk(sorted_arg, 15, dim=2)
+    binned_arg = torch.stack(chunked_arg, dim=2)
+    binned_arg.shape
+
+    """## Calculating confidences"""
+
+    # calculate the mean confidence for each binning range for each
+    confidences = torch.mean(binned_preds, dim=3)
+    confidences.shape
+
+    """## Calculating accuracy for each binning range"""
+
+    differences = torch.abs(binned_arg - binned_gt)
+    nonzero_per_bin = torch.count_nonzero(differences, dim=3)
+
+    acc = nonzero_per_bin / binned_gt.shape[-1]
+    acc.shape
+
+    """## Difference between accuracy and confidences"""
+
+    error = torch.abs(confidences - acc)
+
+    """## Summing across ranges, classes, and examples"""
+
+    range_sum = torch.sum(error, dim=2)
+
+    class_sum = torch.sum(range_sum, dim=1)
+
+    num_sum = torch.sum(class_sum, dim = 0)
+
+    ace = num_sum / (2 * num_classes * num_ranges)
+    return ace
+
 
