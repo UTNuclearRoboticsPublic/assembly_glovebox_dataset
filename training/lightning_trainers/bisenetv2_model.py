4c31b0a5c0288f65bb8ca66c63a239c7bc46ad03
shondle <106922533+shondle@users.noreply.github.com>
2023-08-15 18:20:47 -0500

    Add BiSeNer

---
+++ training/lightning_trainers/bisenetv2_model.py
@@ -0,0 +1,40 @@
+from training.lightning_trainers.lightning_model import LitModel
+from ..models.bisenet_v2 import BiSeNetV2
+import torchmetrics
+import torch.nn.functional as F
+
+
+
+# like UNet, just check the declarations
+class BiSeNetV2Model(LitModel):
+    def __init__(self, learning_rate=0.001, weight_decay=0.1):
+        super(BiSeNetV2Model, self).__init__()
+
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+
+        self.iou = torchmetrics.JaccardIndex(task="multiclass", num_classes=3)
+
+        self.model = BiSeNetV2(n_classes=3)
+
+    def get_loss(self, raw_preds, y):
+        y1, y2 = y
+
+        losses_1 = []
+        losses_2 = []
+        loss = None
+
+        if self.model.training:
+            for preds in raw_preds:
+                print(f"the shape of preds is {preds.shape} and the shape of y1 is {y1.shape}") # [64,3,160,160] [64, 160, 160]
+                loss_1 = F.cross_entropy(preds, y1.long())
+                loss_2 = F.cross_entropy(preds, y2.long())
+                losses_1.append(loss_1)
+                losses_2.append(loss_2)
+            loss = (sum(losses_1) + sum(losses_2)) / 2
+
+        else:
+            loss_1 = F.cross_entropy(raw_preds, y1.long())
+            loss_2 = F.cross_entropy(raw_preds, y2.long())
+            loss = (loss_1 + loss_2) / 2
+        return loss 
