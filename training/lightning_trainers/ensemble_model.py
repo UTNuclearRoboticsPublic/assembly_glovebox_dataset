b785d49ad450e0b75e0dfabb0e54bb18345fca07
shondle <106922533+shondle@users.noreply.github.com>
2024-01-10 05:31:31 -0600

    Refactor ensemble model and add support for different model types

--- training/lightning_trainers/ensemble_model.py
+++ training/lightning_trainers/ensemble_model.py
@@ -6,19 +6,19 @@ from training.lightning_trainers.lightning_model import LitModel
 from training.lightning_trainers.mobilesam_model import MobileSamLitModel
 from training.lightning_trainers.bisenetv2_model import BiSeNetV2Model
 
-
-
-
 from lightning.pytorch import loggers as pl_loggers
 from training.dataloaders.datamodule import AssemblyDataModule
 import lightning.pytorch as pl
 
 import time
+import os
 
 
 class EnsembleModel(LitModel):
     def __init__(self, model_type, test_dropout=False):
         super(EnsembleModel, self).__init__()
+
+        self.model_type = model_type
 
         #TODO: instead of using LitModel, use other one if not using UNET
 
@@ -34,25 +34,25 @@ class EnsembleModel(LitModel):
 
         if model_type == "bisenetv2":
             path_1="./lightning_logs/version_79/checkpoints/epoch=3373-step=26992.ckpt"
-            model_1 = BiSeNetV2Model.load_from_checkpoint(path_1, map_location=torch.device('cuda:0'), test_dropout=test_dropout)
+            model_1 = BiSeNetV2Model.load_from_checkpoint(path_1, map_location=torch.device('cuda:1'), test_dropout=test_dropout)
 
             
             path_2="./lightning_logs/version_77/checkpoints/epoch=1083-step=8672.ckpt"
-            model_2 = BiSeNetV2Model.load_from_checkpoint(path_2, map_location=torch.device('cuda:0'), test_dropout=test_dropout)
+            model_2 = BiSeNetV2Model.load_from_checkpoint(path_2, map_location=torch.device('cuda:1'), test_dropout=test_dropout)
 
             path_3="./lightning_logs/version_72/checkpoints/epoch=1440-step=11528.ckpt"
-            model_3 = BiSeNetV2Model.load_from_checkpoint(path_3, map_location=torch.device('cuda:0'), test_dropout=test_dropout)
+            model_3 = BiSeNetV2Model.load_from_checkpoint(path_3, map_location=torch.device('cuda:1'), test_dropout=test_dropout)
 
         if model_type == "mobilesam":
             
             path_1="./lightning_logs/version_80/checkpoints/epoch=3027-step=24224.ckpt"
-            model_1 = MobileSamLitModel.load_from_checkpoint(path_1, map_location=torch.device('cuda:0'), test_dropout=test_dropout)
+            model_1 = MobileSamLitModel.load_from_checkpoint(path_1, map_location=torch.device('cuda:2'), test_dropout=test_dropout)
             
             path_2="./lightning_logs/version_75/checkpoints/epoch=1849-step=29600.ckpt"
-            model_2 = MobileSamLitModel.load_from_checkpoint(path_2, map_location=torch.device('cuda:0'), test_dropout=test_dropout)
+            model_2 = MobileSamLitModel.load_from_checkpoint(path_2, map_location=torch.device('cuda:2'), test_dropout=test_dropout)
 
             path_3="./lightning_logs/version_69/checkpoints/epoch=1312-step=10704.ckpt"
-            model_3 = MobileSamLitModel.load_from_checkpoint(path_3, map_location=torch.device('cuda:0'), test_dropout=test_dropout)
+            model_3 = MobileSamLitModel.load_from_checkpoint(path_3, map_location=torch.device('cuda:2'), test_dropout=test_dropout)
 
         self.models = [model_1, model_2, model_3]
 
@@ -68,18 +68,26 @@ class EnsembleModel(LitModel):
         predictions = []
         times = []
         for model in self.models:
-            start_time = time.time()
-            output = model.model(x) # access the model in the method
+            start_time = time.perf_counter()
+
+            # use the _common_set method from the model instead if MobileSAM
+            if self.model_type=="mobilesam":
+                _, output = model._common_set(batch, batch_idx)
+            else:
+                output = model.model(x) # access the model in the method
 
              # because bisenet return multiple logits in train mode
             if isinstance(output, tuple):
                 output = output[0]
 
-            end_time = time.time()
+            end_time = time.perf_counter()
             pred_time = end_time - start_time
-            times.append(pred_time / x.shape[0])
+            times.append(pred_time)
+
+            print(f" Time: {pred_time / x.shape[0]}")
             predictions.append(output)
-        self.avg_pred_time = sum(times) / len(times)
+        self.avg_pred_time = sum(times) / x.shape[0] # should just be sum because you are using all of the models
+        print(f"average time is {self.avg_pred_time}")
         super()._set_time(self.avg_pred_time)
         predictions = torch.stack(predictions, dim=-1)
         averaged_predictions = torch.mean(predictions, dim=-1)
@@ -89,11 +97,11 @@ class EnsembleModel(LitModel):
 if __name__ == "__main__":
 
     # TODO: make this a command line argument
-    model_type = "bisenetv2"
-    fast_dev_run = True
+    model_type = "unet"
+    fast_dev_run = False
     
     # TODO: make this a command line argument
-    sets = ["ood", "id", "ood+gs", "gs"]
+    sets = ["ood", "id", "gs", "ood+gs"]
     # Step 1. Load all relevant models in the ensemble.
     # Be consistent, either chose the latest or best performing epoch. Don't combine like I think I did.
 
@@ -106,8 +114,6 @@ if __name__ == "__main__":
     else:
         batch_size = 0
 
-
-    model = EnsembleModel(model_type = model_type)
 
 
     data = {
@@ -156,17 +162,25 @@ if __name__ == "__main__":
 
     for active_set in sets:
         for dropout in [False, True]:
+            model = EnsembleModel(model_type = model_type, test_dropout=dropout)
+            used_data = data.copy()
             if active_set=="ood":
-                data['test_query']['distribution'] = ["ood"]
+                used_data['test_query']['distribution'] = ["ood"]
             if active_set=="id":
-                data['test_query']['distribution'] = ["id"]
+                used_data['test_query']['distribution'] = ["id"]
+                used_data['test_query']['participants'] = ["Test_Subject_2"]
             if active_set=="gs":
-                data['test_query']['distribution'] = ["replaced_green_screen"]
+                used_data['test_query']['distribution'] = ["replaced_green_screen"]
             if active_set=="ood+gs":
-                data['test_query']['distribution'] = ["ood", "replaced_green_screen"]
+                used_data['test_query']['distribution'] = ["ood", "replaced_green_screen"]
 
-            tb_logger = pl_loggers.TensorBoardLogger(save_dir="./data/lightning_logs", name=f"{active_set}_{model_type}_dropout_{dropout}")
+            target_path = f"./lightning_logs/{active_set}_{model_type}_dropout_{dropout}"
 
-            trainer = pl.Trainer(fast_dev_run=fast_dev_run, logger=tb_logger, devices=1, accelerator="gpu")
+            if not os.path.exists(target_path):
+                os.makedirs(target_path)
+
+            tb_logger = pl_loggers.TensorBoardLogger(save_dir="./lightning_logs", name=f"{active_set}_{model_type}_dropout_{dropout}")
+
+            trainer = pl.Trainer(fast_dev_run=fast_dev_run, logger=tb_logger, devices=[0], accelerator="gpu")
 
             trainer.test(model, dm)
